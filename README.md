a4c6941315abe33647dbb2433f827e11d5e11ed6
Paul Mayer <paul@paul-mayer.eu>
2022-03-11 18:54:56 +0100

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# 
+
+## deploy
+
+´´´
+wrangler deploy
+```
+
+## preview
+
+https://workers-kv-from-rust.paul-asvb.workers.dev/
+
+## original init
+```
+wrangler generate workers-kv-from-rust https://github.com/cloudflare/rustwasm-worker-template/
+wrangler kv:namespace create yew_worker_test
+```
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use worker::*;
+
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or("unknown region".into())
+    );
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    log_request(&req);
+    utils::set_panic_hook();
+    let router = Router::new();
+    router
+        .get("/", |_, _| {
+            Response::ok("hello yew worker test".to_string())
+        })
+        .get("/worker-version", |_, ctx| {
+            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
+            Response::ok(version)
+        })
+        .run(req, env)
+        .await
+}
